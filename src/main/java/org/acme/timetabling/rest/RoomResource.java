b78ea514c204bab15a0aeb4cd5c4daf5361f0aef
Nisala Perera <nisalaperera97@gmail.com>
2023-05-17 08:40:42 +0530

    Made changes to RoomResource.java

--- src/main/java/org/acme/timetabling/rest/RoomResource.java
+++ src/main/java/org/acme/timetabling/rest/RoomResource.java
@@ -1,2 +1,32 @@
-package org.acme.timetabling.rest;public class RoomResource {
+package org.acme.timetabling.rest;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.timetabling.domain.Room;
+
+@Path("/rooms")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+public class RoomResource {
+    @POST
+    public Response add(Room room){
+        Room.persist(room);
+        return Response.accepted().build();
+    }
+
+    @DELETE
+    @Path("(roomId)")
+    public Response delete(@PathParam("roomId") Long roomId){
+        Room room = Room.findById(roomId);
+        if (room == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        room.delete();
+        return Response.status(Response.Status.OK).build();
+
+
+    }
 }
