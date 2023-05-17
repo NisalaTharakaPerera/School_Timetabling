66f2bee8ef4cf7ef3727a34ca40f435cb6a7e14f
Nisala Perera <nisalaperera97@gmail.com>
2023-05-17 08:39:55 +0530

    Made changes to TimeTable.java

--- src/main/java/org/acme/timetabling/domain/TimeTable.java
+++ src/main/java/org/acme/timetabling/domain/TimeTable.java
@@ -1,11 +1,23 @@
 package org.acme.timetabling.domain;
 
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+
 import java.util.List;
 
+@PlanningSolution
 public class TimeTable {
-
+    // Timetable doesn't go to the database. But, Lesson, Room, Timeslot goes to the database
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "timeslotRange")
     private List<Timeslot> timeslotList;
+
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "roomRange")
     private List<Room> roomList;
+    @PlanningEntityCollectionProperty
     private List<Lesson> lessonList;
 
     public TimeTable() {
