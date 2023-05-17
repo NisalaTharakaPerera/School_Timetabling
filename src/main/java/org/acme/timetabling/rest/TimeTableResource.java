8c415ffb912effe0036d5690b3b987a0f6c2ee77
Nisala Perera <nisalaperera97@gmail.com>
2023-05-17 08:41:21 +0530

    Made changes to TimeTableResource.java

--- src/main/java/org/acme/timetabling/rest/TimeTableResource.java
+++ src/main/java/org/acme/timetabling/rest/TimeTableResource.java
@@ -6,12 +6,14 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import net.bytebuddy.asm.Advice;
+import org.acme.timetabling.domain.Lesson;
 import org.acme.timetabling.domain.Room;
 import org.acme.timetabling.domain.TimeTable;
 import org.acme.timetabling.domain.Timeslot;
 
 import java.time.DayOfWeek;
 import java.time.LocalTime;
+import java.util.Collections;
 
 @Path("/timeTable")
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,8 +21,14 @@ import java.time.LocalTime;
 public class TimeTableResource {
     @GET
     public TimeTable getTimeTable(){
-        Timeslot timeslot = new Timeslot(DayOfWeek.MONDAY, LocalTime.of(8, 30), LocalTime.of(9, 30));
-        Room room;
-        return new TimeTable();
+//        Timeslot timeslot = new Timeslot(DayOfWeek.MONDAY, LocalTime.of(8, 30), LocalTime.of(9, 30));
+//        Room room = new Room("Room A");
+//        Lesson lesson = new Lesson("Math", "A. Turing", "9th grade");
+//        return new TimeTable(Collections.singletonList(timeslot), Collections.singletonList(room), Collections.singletonList(lesson));
+
+        return new TimeTable(
+                Timeslot.listAll(),
+                Room.listAll(),
+                Lesson.listAll());
     }
 }
